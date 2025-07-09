13ab394e16f6a6a2d09f0c311f75f9e3f1a9bd5a
tsururei <rei.tsurubami@avantcorp.com>
2025-07-09 17:33:34 +0900

    ファイル編集

--- src/main/java/com/example/simplezakka/entity/Cart.java
+++
@@ -1,5 +0,0 @@
-package com.example.simplezakka.entity;
-
-public class Cart {
-    
-}
--- src/main/java/com/example/simplezakka/repository/OrderRepository.java
+++ src/main/java/com/example/simplezakka/repository/OrderRepository.java
@@ -1,5 +1,9 @@
 package com.example.simplezakka.repository;
 
-public class OrderRepository {
-    
+import com.example.simplezakka.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
