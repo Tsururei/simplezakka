7ce8707069e60ae5d7b7491d373773d46b418216
HAN <kennnshuit@gmail.com>
2025-07-11 10:47:32 +0900

    keoeo

--- src/main/java/com/example/simplezakka/controller/AuthController.java
+++ src/main/java/com/example/simplezakka/controller/AuthController.java
@@ -1,7 +1,8 @@
 package com.example.simplezakka.controller;
 
 import com.example.simplezakka.dto.auth.LoginRequest;
-import com.example.simplezakka.dto.auth.LoginResponce;
+import com.example.simplezakka.dto.auth.LoginResponse;
+import com.example.simplezakka.dto.auth.RegisterRequest;
 import com.example.simplezakka.dto.auth.UserSession;
 import com.example.simplezakka.exception.AuthenticationException;
 import com.example.simplezakka.service.AuthService;
@@ -11,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,10 +30,18 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         try {
-            TokenResponce tokens = authService.authenticate(request.getLoginEmail(),request.getLoginPassword());
+            LoginResponse tokens = authService.findUserbyloginEmail(request);
             return ResponseEntity.ok(tokens);
         } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("認証失敗: " + e.getMessage());
         }
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            authService.registerUser(request);
+            
+    }
+    
 }
