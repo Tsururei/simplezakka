aa86e608fb28c31ce40148196f08076f39c84174
HAN <kennnshuit@gmail.com>
2025-07-11 10:45:47 +0900

    eee

--- src/main/java/com/example/simplezakka/dto/auth/RegisterRequest.java
+++ src/main/java/com/example/simplezakka/dto/auth/RegisterRequest.java
@@ -1,15 +1,20 @@
 package com.example.simplezakka.dto.auth;
 
+import org.hibernate.annotations.CurrentTimestamp;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class RegisterRequest {
     @NotBlank(message = "メールアドレス")
     @Email(message = "有効なメールアドレスを入力してください")
-    private String loginEmail;
+    private String registerEmail;
 
     @NotBlank(message = "パスワード")
-    private String loginPassword;
+    private String registerPassword;
+
+    @NotBlank(message = "住所")
+    private String registerAddress;
 }
