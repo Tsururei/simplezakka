db5de66a14ec29c727ac2452ea8d4fc8ee0b404f
HAN <kennnshuit@gmail.com>
2025-07-14 16:27:03 +0900

    hennkou

--- src/main/java/com/example/simplezakka/entity/CartItem.java
+++ src/main/java/com/example/simplezakka/entity/CartItem.java
@@ -7,10 +7,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.cglib.core.Local;
 
 @Entity
-@Table(name = "cart_item")
+@Table(name = "cart")
 @Data
 @NoArgsConstructor
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_itemid")
+    private Integer cartItemId;
 
     @Column(name = "cart_id")
     private Integer cartId;
