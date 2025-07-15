a77f49bbb5039c306ddc26332062dd750bfff04c
tsururei <rei.tsurubami@avantcorp.com>
2025-07-15 17:09:27 +0900

    OrderRequest

--- src/main/java/com/example/simplezakka/dto/order/OrderRequest.java
+++ src/main/java/com/example/simplezakka/dto/order/OrderRequest.java
@@ -2,11 +2,18 @@ package com.example.simplezakka.dto.order;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class OrderRequest {
     @Valid
     @NotNull(message = "顧客情報は必須です")
     private CustomerInfo customerInfo;
+
+    @NotEmpty(message = "注文商品は必須です")
+    private List<OrderItemDto> items;
+
 }
