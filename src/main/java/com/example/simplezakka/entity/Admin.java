fc8684288ff14251dd525773c8f606f2092e0025
YUKlT0 <yukito.asaeda@avantcorp.com>
2025-07-23 11:14:31 +0900

    不要なファイルの整理とバリデーション

--- src/main/java/com/example/simplezakka/entity/Admin.java
+++ src/main/java/com/example/simplezakka/entity/Admin.java
@@ -1,6 +1,8 @@
 package com.example.simplezakka.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,12 +19,16 @@ public class Admin {
     @Column(name = "admin_id", nullable = false, unique = true, updatable = false)
     private String adminId;
 
+    @NotBlank(message = "名前は必須です")
     @Column(name = "admin_name", nullable = false)
     private String adminName;
 
+    @NotBlank(message = "メールアドレスは必須です")
+    @Email(message = "メールアドレスの形式が正しくありません")
     @Column(name = "admin_email", nullable = false, unique = true)
     private String adminEmail;
 
+    @NotBlank(message = "パスワードは必須です")
     @Column(name = "admin_password", nullable = false)
     private String adminPassword;
 
